cd35e7fa5a8f709b1a15770821f1110e33d63670
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-03-10 22:51:11 +0600

    Base template

--- jobs/settings.py
+++ jobs/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobsapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'jobs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jobs.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -79,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -99,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -113,8 +109,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
---
+++ static/js/front.js
@@ -0,0 +1,298 @@
+/*!
+ * Bootstrap Job Board Template
+ * Copyright 2016-2018, Ondrej Svestka
+ */
+
+$(function () {
+
+    map();
+    menuSliding();
+
+    // ------------------------------------------------------- //
+    // Testimonials Slider
+    // ------------------------------------------------------ //
+    $(".testimonials").owlCarousel({
+        nav: false,
+        dots: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
+
+    // ------------------------------------------------------- //
+    // Bootstrap Select
+    // ------------------------------------------------------ //
+    $('.select2').selectpicker();
+
+});
+
+// ------------------------------------------------------- //
+// Styled Google Map
+// ------------------------------------------------------ //
+
+function map() {
+
+    if ($('#map').length > 0) {
+
+
+        function initMap() {
+
+            var location = new google.maps.LatLng(50.0875726, 14.4189987);
+
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+                center: location,
+                zoom: 16,
+                panControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+
+            var markerImage = 'img/marker.png';
+
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: markerImage
+            });
+
+            var contentString = '<div class="info-window">' +
+                '<h3>Info Window Content</h3>' +
+                '<div class="info-content">' +
+                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                '</div>' +
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+                maxWidth: 400
+            });
+
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
+            var styles = [{
+                "featureType": "landscape",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "lightness": 65
+                }, {
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "poi",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "lightness": 51
+                }, {
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "road.arterial",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "lightness": 30
+                }, {
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "road.local",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "lightness": 40
+                }, {
+                    "visibility": "on"
+                }]
+            }, {
+                "featureType": "transit",
+                "stylers": [{
+                    "saturation": -100
+                }, {
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "administrative.province",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "on"
+                }, {
+                    "lightness": -25
+                }, {
+                    "saturation": -100
+                }]
+            }, {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                    "hue": "#ffff00"
+                }, {
+                    "lightness": -25
+                }, {
+                    "saturation": -97
+                }]
+            }];
+
+            map.set('styles', styles);
+
+
+        }
+
+        google.maps.event.addDomListener(window, 'load', initMap);
+
+
+    }
+
+}
+
+
+// ------------------------------------------------------- //
+// Sliding dropdowns
+// ------------------------------------------------------ //
+
+function menuSliding() {
+
+
+    var dropdowns = $('.dropdown');
+
+    dropdowns.on('show.bs.dropdown', function (e) {
+
+            var dropdownMenu = $(this).find('.dropdown-menu').first();
+
+            if ($(window).width() > 750) {
+                dropdownMenu.stop(true, true).slideDown();
+
+            } else {
+                dropdownMenu.stop(true, true).show();
+            }
+        }
+
+    );
+    dropdowns.on('hide.bs.dropdown', function (e) {
+
+        var dropdownMenu = $(this).find('.dropdown-menu').first();
+
+        if ($(window).width() > 750) {
+            dropdownMenu.stop(true, true).slideUp();
+        } else {
+            dropdownMenu.stop(true, true).hide();
+        }
+    });
+
+}
+
+// ------------------------------------------------------- //
+// Utilities - tooltips, external links, scroll-to links
+// ------------------------------------------------------ //
+
+function utils() {
+
+    /* tooltips */
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* click on the box activates the radio */
+
+    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function (e) {
+        var radio = $(this).find(':radio');
+        radio.prop('checked', true);
+    });
+    /* click on the box activates the link in it */
+
+    $('.box.clickable').on('click', function (e) {
+
+        window.location = $(this).find('a').attr('href');
+    });
+    /* external links in new window*/
+
+    $('.external').on('click', function (e) {
+
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+    /* animated scrolling */
+
+    $('.scroll-to, .scroll-to-top').click(function (event) {
+
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        if (parts.length > 1) {
+
+            scrollTo(full_url);
+            event.preventDefault();
+        }
+    });
+
+    function scrollTo(full_url) {
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+        var target_offset = $("#" + trgt).offset();
+        var target_top = target_offset.top - 100;
+        if (target_top < 0) {
+            target_top = 0;
+        }
+
+        $('html, body').animate({
+            scrollTop: target_top
+        }, 1000);
+    }
+}
+
+// ------------------------------------------------------ //
+// For demo purposes, can be deleted
+// ------------------------------------------------------ //
+
+var stylesheet = $('link#theme-stylesheet');
+$("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
+var alternateColour = $('link#new-stylesheet');
+
+if ($.cookie("theme_csspath")) {
+    alternateColour.attr("href", $.cookie("theme_csspath"));
+}
+
+$("#colour").change(function () {
+
+    if ($(this).val() !== '') {
+
+        var theme_csspath = 'css/style.' + $(this).val() + '.css';
+
+        alternateColour.attr("href", theme_csspath);
+
+        $.cookie("theme_csspath", theme_csspath, {
+            expires: 365,
+            path: document.URL.substr(0, document.URL.lastIndexOf('/'))
+        });
+
+    }
+
+    return false;
+});
