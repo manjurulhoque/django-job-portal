8c6ed56df8e6fe3ab00766e1ba50c457e233a18f
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-04-10 21:29:27 +0600

    Added requirements for heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jobs.wsgi --log-file -
--- jobs/settings.py
+++ jobs/settings.py
@@ -117,6 +117,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
