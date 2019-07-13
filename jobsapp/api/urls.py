eee1b9bd5842f506e24dfcea80f9e345e43e55fe
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-07-13 17:51:13 +0600

    Simple changes

--- jobsapp/api/urls.py
+++ jobsapp/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import JovViewSet
+from .views import JobViewSet
 
 router = DefaultRouter()
-router.register('jobs', JovViewSet, base_name='jobs')
+router.register('jobs', JobViewSet, base_name='jobs')
 
 urlpatterns = []
 
--- jobsapp/api/views.py
+++ jobsapp/api/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets, mixins, status
 from .serializers import *
 
 
-class JovViewSet(viewsets.ReadOnlyModelViewSet):
+class JobViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = JobSerializer
     queryset = serializer_class.Meta.model.objects.all()
