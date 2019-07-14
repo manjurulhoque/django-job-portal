8519a5c53f86981aec2308baf1877ebbab4b1b01
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-07-14 16:21:40 +0600

    Simple changes

--- jobs/settings.py
+++ jobs/settings.py
@@ -33,8 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
     'jobsapp',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
