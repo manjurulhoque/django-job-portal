3f82b0225fae948ce7db85973d800c9b5846a122
Rumi <todoyel19@gmail.com>
2019-12-10 10:50:32 +0600

    Host allowed

--- jobs/settings.py
+++ jobs/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-ALLOWED_HOSTS = ['django-portal.herokuapp.com', 'localhost', 'jobs.manjurulhoque.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-portal.herokuapp.com', 'localhost', 'jobs.manjurulhoque.com', '127.0.0.1', 'localhost:3000']
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
