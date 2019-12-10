78f2bf62b627fa73af6d2d1887bdf9c1964c814f
Rumi <todoyel19@gmail.com>
2019-12-10 10:59:20 +0600

    Host allowed

--- jobs/settings.py
+++ jobs/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-ALLOWED_HOSTS = ['django-portal.herokuapp.com', 'localhost', 'jobs.manjurulhoque.com', '127.0.0.1', 'localhost:3000']
+# ALLOWED_HOSTS = ['django-portal.herokuapp.com', 'localhost', 'jobs.manjurulhoque.com', '127.0.0.1', 'localhost:3000']
+ALLOWED_HOSTS = ['*']
 # cors config
 CORS_ORIGIN_ALLOW_ALL = True
 
