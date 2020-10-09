7aa12751a433acb5f49d7056ed069bc179f61d4a
Oriol Piera <oriol.piera@somenergia.coop>
2020-10-09 20:28:10 +0200

    Add setup pytest

--- jobsapp/tests.py
+++ jobsapp/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase  # noqa
+import factory
+from jobsapp.models import User, Job
+import pytest
+
+
+# List of factories
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    first_name = 'John'
+    last_name = 'Doe'
+    admin = False
+
+
+class JobFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Job
+        django_get_or_create = ('type')
+
+    user = factory.SubFactory('jobsapp.tests.UserFactory')
+    title = factory.Sequence(lambda n: 'Title %d' % n)
+    description = factory.Sequence(lambda n: 'Description %d' % n)
+    type = '1'
+
 
 # Create your tests here.
+@pytest.mark.django_db
+class TestExampe:
+    def test__exemple__ok(self):
+        x = "my simple app test"
+        assert 'simple app' in x
