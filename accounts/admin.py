c062c2a587288c7dcc779a6292290431294f677d
Xavier Diumé <xdiume@gmail.com>
2020-10-13 21:55:35 +0200

    First approach to get models in adminsite

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin  # noqa
 
 # Register your models here.
+from accounts.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "gender",
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+    ]
+    list_filter = ["role", "gender", "is_active", "is_staff", "is_superuser"]
+    fields = [
+        "email",
+        "first_name",
+        "last_name",
+        "gender",
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+    ]
+
+
+admin.site.register(User, UserAdmin)
--- jobsapp/admin.py
+++ jobsapp/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin  # noqa
 
 # Register your models here.
+from jobsapp.models import Job, Applicant
+
+
+class ApplicantInline(admin.TabularInline):
+    model = Applicant
+    extra = 1
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "salary",
+        "location",
+        "type",
+        "category",
+        "company_name",
+        "last_date",
+        "created_at",
+        "filled",
+        "user",
+    ]
+    list_filter = ["salary", "last_date", "created_at", "user"]
+    date_hierarchy = "created_at"
+    inlines = [ApplicantInline]
+
+
+admin.site.register(Job, JobAdmin)
+
+
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ["user", "job", "created_at"]
+    list_filter = ["user", "created_at"]
+    date_hierarchy = "created_at"
+
+
+admin.site.register(Applicant, ApplicantAdmin)
