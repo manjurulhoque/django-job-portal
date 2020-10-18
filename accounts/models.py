9d7c349c925f133aedbec1834bdd89955d989f8b
xdiume <72464185+xdiume@users.noreply.github.com>
2020-10-18 10:01:57 +0200

    Model fields info (#71)
    * Information for models in jobsapp
    
    * Added information in account models and other formatting changes
    
    * Provide related model migrations
    
    * Fix whiteline ^^'
    
    Co-authored-by: Xavi Torelló <info@xaviertorello.cat>

--- accounts/models.py
+++ accounts/models.py
@@ -2,28 +2,45 @@ from typing import List
 
 from django.contrib.auth.models import AbstractUser  # type: ignore
 from django.db import models  # type: ignore
+from django.utils.translation import ugettext as _
 
 from accounts.managers import UserManager
 
-GENDER_CHOICES = (("male", "Male"), ("female", "Female"))
+GENDER_MALE = "male"
+GENDER_FEMALE = "female"
+GENDER_CHOICES = ((GENDER_MALE, _("Male")), (GENDER_FEMALE, _("Female")))
 
 
 class User(AbstractUser):
     username = None
-    role = models.CharField(max_length=12, error_messages={"required": "Role must be provided"})
-    gender = models.CharField(max_length=10, blank=True, null=True, default="")
+    role = models.CharField(
+        max_length=12,
+        error_messages={"required": "Role must be provided"},
+        verbose_name=_("Role"),
+        help_text=_("User role."),
+    )
+    gender = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True,
+        default="",
+        verbose_name=_("Gender"),
+        help_text=_("User gender."),
+    )
     email = models.EmailField(
         unique=True,
         blank=False,
         error_messages={
-            "unique": "A user with that email already exists.",
+            "unique": _("A user with that email already exists."),
         },
+        verbose_name=_("Email"),
+        help_text=_("User email, also used as username."),
     )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS: List[str] = []
 
-    def __unicode__(self):
+    def __str__(self):
         return self.email
 
     objects = UserManager()
--- jobsapp/models.py
+++ jobsapp/models.py
@@ -1,7 +1,12 @@
+# DJANGO Imports
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext as _
 
+# APP Imports
 from accounts.models import User
+
+# Global Imports
 
 
 class Job(models.Model):
@@ -9,34 +14,81 @@ class Job(models.Model):
     JOB_TYPE_PART_TIME = "2"
     JOB_TYPE_INTERNSHIP = "3"
     JOB_TYPES = (
-        (JOB_TYPE_FULL_TIME, "Full time"),
-        (JOB_TYPE_PART_TIME, "Part time"),
-        (JOB_TYPE_INTERNSHIP, "Internship"),
+        (JOB_TYPE_FULL_TIME, _("Full time")),
+        (JOB_TYPE_PART_TIME, _("Part time")),
+        (JOB_TYPE_INTERNSHIP, _("Internship")),
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=300)
-    description = models.TextField()
-    location = models.CharField(max_length=150)
-    type = models.CharField(choices=JOB_TYPES, max_length=10)
-    category = models.CharField(max_length=100)
-    last_date = models.DateTimeField()
-    company_name = models.CharField(max_length=100)
-    company_description = models.CharField(max_length=300)
-    website = models.CharField(max_length=100, default="")
-    created_at = models.DateTimeField(default=timezone.now)
-    filled = models.BooleanField(default=False)
-    salary = models.IntegerField(default=0, blank=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
+        help_text=_("User who creates this job."),
+    )
+    title = models.CharField(
+        max_length=300, verbose_name=_("Title"), help_text=_("Short job title.")
+    )
+    description = models.TextField(
+        verbose_name=_("Description"), help_text=_("Long job description.")
+    )
+    location = models.CharField(
+        max_length=150, verbose_name=_("Location"), help_text=_("Location for this job position.")
+    )
+    type = models.CharField(
+        choices=JOB_TYPES, max_length=10, verbose_name=_("Type"), help_text=_("Job type.")
+    )
+    category = models.CharField(
+        max_length=100, verbose_name=_("Category"), help_text=_("Category clasification.")
+    )
+    last_date = models.DateTimeField(verbose_name=_("Last date"), help_text=_("Last date."))
+    company_name = models.CharField(
+        max_length=100, verbose_name=_("Company"), help_text=_("Job's Company name.")
+    )
+    company_description = models.CharField(
+        max_length=300,
+        verbose_name=_("Company description"),
+        help_text=_("Company description, activity,..."),
+    )
+    website = models.CharField(
+        max_length=100, default="", verbose_name=_("Website"), help_text=_("Company Website URL.")
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now, verbose_name=_("Created"), help_text=_("Job creation date and time.")
+    )
+    filled = models.BooleanField(
+        default=False, verbose_name=_("Filled"), help_text=_("Job position is filled.")
+    )
+    salary = models.IntegerField(
+        default=0, blank=True, verbose_name=_("Salary"), help_text=_("Maximum salary for this job.")
+    )
+
+    class Meta:
+        verbose_name = _("Job")
+        verbose_name_plural = _("Jobs")
 
     def __str__(self):
         return self.title
 
 
 class Applicant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="applicants")
-    created_at = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name=_("User"), help_text=_("User applicant.")
+    )
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE,
+        related_name="applicants",
+        verbose_name=_("Job"),
+        help_text=_("Job in applicant."),
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name=_("Created"),
+        help_text=_("Applicant creation date and time."),
+    )
 
     class Meta:
+        verbose_name = _("Applicant")
+        verbose_name_plural = _("Applicants")
         unique_together = ("user", "job")
 
     def __str__(self):
