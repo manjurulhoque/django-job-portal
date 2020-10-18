1f3aeda790ec6a45256c0fffd38f03e4d0be7f3b
fullonic <somnium@riseup.net>
2020-10-18 21:22:03 +0200

    Removes more mypy erros

--- jobsapp/api/views/employer.py
+++ jobsapp/api/views/employer.py
@@ -1,5 +1,9 @@
+from typing import Generic, TypeVar
+
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+
+from django.db.models import QuerySet
 
 from jobsapp.api.permissions import IsEmployer
 from jobsapp.api.serializers import ApplicantSerializer
@@ -10,6 +14,6 @@ class ApplicantsListAPIView(ListAPIView):
     serializer_class = ApplicantSerializer
     permission_classes = [IsAuthenticated, IsEmployer]
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         user = self.request.user
         return Applicant.objects.filter(job__user_id=user.id)
