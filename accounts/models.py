c532d48b7cb16728270825ebcdc89ae901cb969f
fullonic <somnium@riseup.net>
2020-10-18 19:24:24 +0200

    Reduce mypy errors .
    Note:
    In managers.py we ignore some error on lines [30, 31, 39, 40, 42, 44] due some strange behavior.
    When reproduced on mypy playground, there are no errors
    https://mypy-play.net/?mypy=latest&python=3.8&flags=strict%2Cwarn-unreachable&gist=d83e3271298100a3240195d75c8c6cfe

--- accounts/models.py
+++ accounts/models.py
@@ -41,7 +41,7 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS: List[str] = []
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.email
 
     # TODO: Solve mypy error later:
