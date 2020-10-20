724567989d56bfb7b8e48635c4a9068b2fd114db
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2020-10-21 00:31:25 +0600

    Accounts model test

---
+++ tests/accounts/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from accounts.models import User
+
+
+class UserModelTest(TestCase):
+
+    def setUp(self) -> None:
+        self.valid_user = {
+            "first_name": "Manjurul",
+            "last_name": "Hoque",
+            "role": "employee",
+            "gender": "male",
+            "email": "rumi1@gmail.com",
+            "password": "123456",
+        }
+        self.user = User.objects.create(**self.valid_user)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.user), self.user.email)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(User._meta.verbose_name_plural), "users")
+
+    def test_full_name(self):
+        self.assertEqual(self.user.get_full_name(), "Manjurul Hoque")
+
+    def test_email_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email')
