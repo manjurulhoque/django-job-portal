fdd017f0893582ee6ebd078e1a9b346a14b2bfef
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2020-10-29 01:39:30 +0600

    Test job model

--- jobsapp/models.py
+++ jobsapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 from accounts.models import User
@@ -24,6 +25,9 @@ class Job(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     filled = models.BooleanField(default=False)
     salary = models.IntegerField(default=0, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('jobs:jobs-detail', args=[self.id])
 
     def __str__(self):
         return self.title
---
+++ tests/jobsapp/test_models.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+
+from django.test import TestCase
+
+from accounts.models import User
+from jobsapp.models import Job
+
+
+class TestJobModel(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.valid_job = {
+            "title": "Junior Software Engineer",
+            "description": "Looking for Python developer",
+            "salary": 35000,
+            "location": "Dhaka, Bangladesh",
+            "type": "1",
+            "category": "web-development",
+            "last_date": datetime.now() + timedelta(days=30),
+            "company_name": "Dev Soft",
+            "company_description": "A foreign country",
+            "website": "www.devsoft.com",
+        }
+        cls.employer = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "employer@gmail.com",
+            "role": "employer",
+            "password": "123456",
+        }
+        cls.user = User.objects.create(**cls.employer)
+        cls.job = Job(**cls.valid_job)
+        cls.job.user = cls.user
+        cls.job.save()
+
+    def test_get_absolute_url(self):
+        self.assertURLEqual(self.job.get_absolute_url(), '/jobs/1')
+
+    def test_title_max_length(self):
+        max_length = self.job._meta.get_field('title').max_length
+        self.assertEqual(max_length, 300)
+
+    def test_title_label(self):
+        field_label = self.job._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
