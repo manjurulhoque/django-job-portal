69f9b65ae150b8875cfb142ef16b9b3fa57d8137
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-12-26 23:47:35 +0600

    JWT auth using graphQL

--- accounts/graphql/mutations.py
+++ accounts/graphql/mutations.py
@@ -1,7 +1,11 @@
 import graphene
+import graphql_jwt
 from . import sub_mutations as user_mutations
 
 
 class AuthMutation(graphene.ObjectType):
+    login = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
     employee_register = user_mutations.EmployeeRegister.Field()
     employer_register = user_mutations.EmployerRegister.Field()
