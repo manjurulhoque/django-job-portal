f24426e7e067a8ece718953dd02f67bc4320bfc7
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2023-01-23 00:06:53 +0600

    Updated dockerfile

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/gunicorn jobs.wsgi:application -w 2 -b :8000
+#/usr/local/bin/gunicorn jobs.wsgi:application -w 2 -b :8000
 
 #gunicorn jobs.wsgi:application --bind 0.0.0.0:8000
