7bc7a85782aac5b9682321a6fad7cf96f12aee66
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2023-01-23 20:23:44 +0600

    Updated settings

--- jobs/settings.py
+++ jobs/settings.py
@@ -159,8 +159,8 @@ CORS_ALLOW_HEADERS = (
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "jobs/staticfiles")  # for production
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "jobs/mediafiles")
