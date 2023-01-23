f35d0269e2f28da5a5c6cbdaae535561b553e622
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2023-01-23 20:34:26 +0600

    Updated settings

--- jobs/wsgi.py
+++ jobs/wsgi.py
@@ -6,4 +6,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobs.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+# application = WhiteNoise(application)
