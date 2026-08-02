b9290f8dc2c347b536365cd82703fb15838f5a28
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2026-08-03 00:43:44 +0600

    fix: Correct admin.py errors

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
